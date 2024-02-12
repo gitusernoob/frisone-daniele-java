3caa24df7fefa8cf034aed408bd504dcf992d419
Daniele Frisone <daniele.frisone@hotmail.it>
2024-02-12 22:58:11 +0100

    esercizi martedi 13

--- lezione1/lezione3/Esercizi.java
+++ lezione1/lezione3/Esercizi.java
@@ -1,5 +1,6 @@
 package lezione3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Esercizi {
@@ -28,6 +29,7 @@ public class Esercizi {
         int tempMin = 99999;
         int tempMax = -99999;
         int sommatemp = 0;
+        ArrayList<Integer> array = new ArrayList<>();
 
         for (int i = 0; i <nVolte; i++){
             System.out.println("inserisci temperatura");
@@ -35,6 +37,7 @@ public class Esercizi {
             if (temperatura < tempMin) tempMin = temperatura;
             if (temperatura > tempMax) tempMax = temperatura;
             sommatemp += temperatura;
+            array.add(temperatura);
         }
         System.out.println("la temperatura media è " + sommatemp/nVolte);
         System.out.println("la temperatura max è " + tempMax);
@@ -42,15 +45,50 @@ public class Esercizi {
 
 
         // es3) (for arraylist) salvare le temp precedenti. stampare quelle maggiori di 10 gradi
-
+        System.out.println("le temperature maggiori di 10 sono: ");
+        for (int i = 0; i< array.size(); i++){
+            if (array.get(i)>=10){
+                System.out.println(array.get(i));
+            }
+        }
         
         // es4) (while) chiede all utente dei numeri e sommarli finche non scrive 0 (termina inserisce 0)
+        System.out.println("scrivi 0 per terminare il nuovo inserimento di numeri. il while li somma: ");
+        int somma = -1;
+        int numeroDaSommare = -1;
+        int c = 0;
+        while (numeroDaSommare != 0){
+
+            if(c==0){
+                numeroDaSommare=0;
+                c=1;
+                System.out.println("ho azzerato numero da sommare");
+            }
+            numeroDaSommare = Integer.parseInt(tastiera.nextLine());
+            somma += numeroDaSommare;
+            System.out.println("il tot per adesso è : " + somma);
+            if(numeroDaSommare == 0){
+                System.out.println("esco dal programma");
+            }
+        }
 
 
         // es5) (string substring) stampare il proprio nome al contrario
 
+        System.out.println("inserisci il tuo nome");
+        String stringa =  tastiera.nextLine();
+        char[] stringaInvertita = stringa.toCharArray();
+        int c1 = 0;
+        for (int i = stringa.length() - 1; i >= 0; i--) {
+            stringaInvertita[c] =stringa.charAt(i);
+            c1++;
+        }
+        System.out.println(stringaInvertita);
+
 
 
     }
+
+
 
 }
